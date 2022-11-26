e4626d3f976eb41a3f71042a56129031c0e538f8
unknown <osama_magdy0@yahoo.com>
2022-11-26 08:07:33 +0200

    the final js fil addtion

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const stepsEl = document.querySelectorAll("h3");
 const h1El = document.querySelector("h1");
 
-const pEl = document.querySelectorAll("p");
+const pEl = document.querySelectorAll(".par");
+const printEl = document.querySelector(".print");
 
-console.log();
+// console.log();
+
 stepsEl.forEach((step) => {
   step.onclick = () => {
     stepsEl.forEach((step) => {
@@ -13,10 +15,14 @@ stepsEl.forEach((step) => {
     h1El.innerHTML = `Step ${step.innerText.slice(4, 7)}`;
     // second for Each to add active class to target step
     step.classList.add("active");
-    console.log(step.innerHTML.charAt(5));
-    console.log(h1El.innerHTML.charAt(6));
-    // pEl.forEach((par) => {
-    //   par.classList.add("display");
-    // });
+    printEl.innerHTML = ``;
+    if (step.innerHTML.charAt(5) === h1El.innerHTML.charAt(6)) {
+      pEl.forEach((pp) => {
+        pp.classList.add("display");
+        pp.classList.add("print");
+        pp.classList.remove("par");
+      });
+    }
+    pEl[+step.innerHTML.charAt(5) - 1].classList.remove("display");
   };
 });
